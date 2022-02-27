1d87c705a499cfb9d8cc9847623afca12fda75d3
nami <warriorparovoz@gmail.com>
2022-02-27 21:10:16 +0300

    End..

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header class="header">
+    <?php
+        echo '<img class="header__logo" src="images/logo.png">
+            <h1 class="header__title">Задание для самостоятельной работы «Hello, World!»</h1>'
+    ?>
+</header>
+<main class="main">
+    <?php
+        echo '<p class="main__text">Hello, world</p>'
+    ?>
+</main>
+<footer class="footer">
+    <?php
+        echo '<p class="footer__text">Здесь могла быть ваша реклама</p>'
+    ?>
+</footer>
+</body>
+</html>
